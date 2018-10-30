e8df1ef44c8bbd2028c6ebc8d8c886937d8e1a50
GBTC440018UR <GBTC440018UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-30 15:28:28 -0400

    completed

---
+++ src/main/java/com/example/demo/Actor.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String realname;
+    private String description;
+
+    @ManyToMany(mappedBy = "cast")
+    private Set<Movie> movies;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+}
