8ce1566b5773329d08d937caab7dcfb057981a89
Irina <irinamiranda@github.com>
2018-11-01 08:52:59 -0400

    ManyToMany

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -19,25 +19,48 @@ public class HomeController {
     @RequestMapping("/")
     public String index(Model model){
         //create an actor
-        Actor actor = new Actor();
-        actor.setName("Sandra Bullock");
-        actor.setRealname("Sandra Mae Bullowski");
+        Actor actor1 = new Actor();
+        actor1.setName("Sandra Bullock");
+        actor1.setRealname("Sandra Mae Bullowski");
+
+        Actor actor2 = new Actor();
+        actor2.setName("Sam Adams");
+        actor2.setRealname("Adam Sandler");
 
         //create a movie
-        Movie movie = new Movie();
-        movie.setTitle("Emoji Movie");
-        movie.setYear(2017L);
-        movie.setDescription("About Emojies...");
+        Movie movieEmoji = new Movie();
+        movieEmoji.setTitle("Emoji Movie");
+        movieEmoji.setYear(2017L);
+        movieEmoji.setDescription("About Emojies...");
+
+        Movie movieTitanic = new Movie();
+        movieTitanic.setTitle("Titanic Movie");
+        movieTitanic.setYear(2017L);
+        movieTitanic.setDescription("About Titanic...");
 
         //add the movie to an empty list
-        Set<Movie> movies = new HashSet<Movie>();
-        movies.add(movie);
+        Set<Movie> movies1 = new HashSet<Movie>();
+        movies1.add(movieEmoji);
+        movies1.add(movieTitanic);
 
         //add the list of movies to the actor's movie list
-        actor.setMovies(movies);
+        actor1.setMovies(movies1);
 
         //save the actor to the database
-        actorRepository.save(actor);
+        actorRepository.save(actor1);
+
+
+        Set<Movie> movies2 = new HashSet<Movie>();
+        movies2.add(movieTitanic);
+
+        //add the list of movies to the actor's movie list
+        actor2.setMovies(movies2);
+
+        //save the actor to the database
+        actorRepository.save(actor2);
+
+
+
 
         //Grad all the actors to the database and send them to the template
         model.addAttribute("actors", actorRepository.findAll());
